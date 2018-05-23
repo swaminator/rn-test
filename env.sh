3c69eb07d571f1cb5bb698eaf614aa0dbcaf775b
Nikhil Swaminathan <nsswamin@amazon.com>
2018-05-23 08:40:45 -0700

    add buildspec for code build

---
+++ env.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env sh
+
+# Use this file to specify new custom environment variables or modify existing
+# environment variables on your build instance. This file is executed by the build
+# instance at the start of your build as set by the buildspec.yml file.
+#
+# You can specify custom environment variables
+# e.g. BUILD_TYPE="test"
+#
+# You can also modify existing environment variables on the build instance
+# e.g. PATH="$PATH:$ANDROID_HOME/build-tools/26.0.3"
